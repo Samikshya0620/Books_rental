6f0ee310fe2d03c5271e491cc5ca139bd00d9973
Sagar Sharma <sharma.sgr250@gmail.com>
2023-02-19 23:16:09 +0545

    cartContextErrorRemains

--- rentalfrontend/src/App.jsx
+++ rentalfrontend/src/App.jsx
@@ -9,27 +9,32 @@ import Login from "./pages/Login";
 import ProductPage from "./pages/ProductPage";
 import Register from "./pages/Register";
 import { ProductProvider } from "./context/productsContext";
+import { CartProvider } from "./context/cartContext";
 const Layout = () => {
   return <Outlet />;
 };
 
+
+
 function App() {
   return (
-    <ProductProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="register" element={<Register />} />
-            <Route path="product" element={<ProductPage />} />
-            <Route path="login" element={<Login />} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="category" element={<CategoryPage />} />
-            <Route path="*" element={<Navigate to={"/"} replace />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ProductProvider>
+    <CartProvider>
+      <ProductProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="register" element={<Register />} />
+              <Route path="product" element={<ProductPage />} />
+              <Route path="login" element={<Login />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="category" element={<CategoryPage />} />
+              <Route path="*" element={<Navigate to={"/"} replace />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ProductProvider>
+    </CartProvider>
   );
 }
 export default App;
